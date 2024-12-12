c7acc4a9a582f50f07c50357823037dd7417d8d7
Sheraz <sherazarshad419@gmail.com>
2024-12-12 16:03:02 +0500

    layouts added

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
-<script setup>
-const value = ref("");
-</script>
-
 <template>
-  <div class="m-16 space-y-8">
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <Button label="Verify" size="small" />
-
-    <FloatLabel variant="on">
-      <InputText id="username" v-model="value" />
-      <label for="username">Username</label>
-    </FloatLabel>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
