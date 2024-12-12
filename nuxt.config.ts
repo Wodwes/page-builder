d7a76430e2c2266a8e4e447bfc81cc0c0ce543c5
Sheraz <sherazarshad419@gmail.com>
2024-12-12 12:06:32 +0500

    working tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@primevue/nuxt-module"],
+  css: ["~/assets/css/main.css"],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
   primevue: {
     components: {
       prefix: "PV",
