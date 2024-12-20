22e3b9fb9e25670ee285daefd4f0aaf257687ef5
Sheraz <sherazarshad419@gmail.com>
2024-12-20 18:16:01 +0500

    new layut

--- components/components/ButtonComponent.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const props = defineProps(['title']);
-</script>
-
-<template>
-  <div>
-    <button class="w-full rounded-md border bg-white py-2 text-center">{{ title }}</button>
-  </div>
-</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import RightSidebar from '../components/RightSidebar/RightSidebar.vue';
-import { Icon } from '@iconify/vue';
-useSeoMeta({
-  title: 'Page Builder | Wodwes',
-});
+useSeoMeta({ title: 'Page Builder | Wodwes' });
 
 // State for toggling sidebar visibility
 const isSidebarVisible = ref(true);
@@ -19,7 +14,7 @@ const isOpen = ref(false);
 <template>
   <div class="flex h-auto min-h-screen">
     <!-- Sidebar -->
-    <div v-if="isSidebarVisible" class="hidden w-64 flex-col border-r md:flex dark:border-gray-800">
+    <div v-if="isSidebarVisible" class="w-64 flex-col border-r md:flex dark:border-gray-800">
       <div class="flex h-16 items-center justify-center border-b dark:border-gray-800">
         <span class="text-center leading-4">
           <span class="text-primary font-bold uppercase tracking-widest">Wodwes</span>
@@ -27,27 +22,14 @@ const isOpen = ref(false);
           <span class="text-xs">Page Builder</span>
         </span>
       </div>
+      <!-- sidebar -->
       <SidebarMain />
-    </div>
-
-    <!-- Mobile Sidebar (Slideover from the left) -->
-    <div class="block md:hidden">
-      <USlideover v-model="isOpen" prevent-close>
-        <UCard class="relative left-0 flex flex-1 flex-col" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800 ' }">
-          <template #header>
-            <div class="flex items-end justify-end">
-              <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
-            </div>
-            <SidebarMain />
-          </template>
-        </UCard>
-      </USlideover>
     </div>
 
     <!-- Main Layout -->
     <div class="flex flex-1 flex-col overflow-y-auto">
       <!-- Top Navigation -->
-      <div class="max-md:shadow-sm sticky top-0 z-10 flex min-h-16 items-center justify-between border-b dark:border-gray-800 dark:bg-[#0F172A]">
+      <div class="max-md:shadow-sm sticky top-0 z-10 flex min-h-16 items-center justify-between border-b duration-200 dark:border-gray-800 dark:bg-[#0F172A]">
         <div class="flex items-center px-4">
           <button @click="toggleSidebar" class="z-10 hidden text-gray-500 focus:text-gray-700 focus:outline-none md:block">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -64,14 +46,8 @@ const isOpen = ref(false);
       </div>
 
       <!-- Page Content -->
-      <div class="flex h-auto">
-        <div class="w-full p-4">
-          <slot />
-        </div>
-        <!-- Right Sidebar -->
-        <div class="hidden h-auto min-h-screen w-80 border-l md:max-w-80 lg:flex dark:bg-gray-900">
-          <RightSidebar />
-        </div>
+      <div class="w-full p-4">
+        <slot />
       </div>
     </div>
   </div>
