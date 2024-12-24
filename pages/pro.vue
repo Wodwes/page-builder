27148cc351c117ed255df6b30f374107b27d5745
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-24 16:29:25 +0500

    aside completed

---
+++ pages/pro.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Aside from '~/components/pro/Aside.vue';
+</script>
+<template>
+  <div class="flex h-auto w-full items-center justify-center">
+    <Aside />
+  </div>
+</template>
