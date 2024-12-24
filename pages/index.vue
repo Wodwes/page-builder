8b6011e391b4204d310bf9e14b7cc77399d63e04
Sheraz <sherazarshad419@gmail.com>
2024-12-24 13:53:39 +0500

    tailwind v4 done

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <h1>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
-      eligendi alias mollitia facere assumenda. Sint, saepe? Aut animi sed,
-      earum sapiente pariatur deserunt vitae, perspiciatis enim architecto
-      tempore nemo labore!
-    </h1>
+  <div class="text-primary">
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis eligendi alias mollitia facere assumenda. Sint, saepe? Aut animi sed, earum sapiente pariatur deserunt vitae, perspiciatis enim architecto tempore nemo labore!</h1>
+    <UButton>Test button</UButton>
+    <UAlert
+      title="Heads up!"
+      description="You can change the primary color in your app config."
+      icon="i-lucide-rocket"
+      :ui="{
+        icon: 'size-11',
+      }"
+    />
   </div>
 </template>
