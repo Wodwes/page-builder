81c4511a02213b7e407f4e2386e2551e6eaf20b1
Sheraz <sherazarshad419@gmail.com>
2024-12-24 13:45:11 +0500

    app setted up

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+      eligendi alias mollitia facere assumenda. Sint, saepe? Aut animi sed,
+      earum sapiente pariatur deserunt vitae, perspiciatis enim architecto
+      tempore nemo labore!
+    </h1>
+  </div>
+</template>
