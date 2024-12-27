f965bb62aa895caeb1464667153e589e14fae5f7
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-27 11:19:26 +0500

    add icon component

--- pages/pro.vue
+++ pages/pro.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Aside from '~/components/pro/Aside.vue';
-import AuthForm from '~/components/pro/AuthForm.vue';
+import AuthForm from '~/components/pro/forms/AuthForm.vue';
 import Banner1 from '~/components/pro/banners/Banner1.vue';
 import Banner2 from '~/components/pro/banners/Banner2.vue';
 import Banner3 from '~/components/pro/banners/Banner3.vue';
@@ -10,6 +10,8 @@ import LightBoxGallery from '~/components/pro/gallery/LightBoxGallery.vue';
 import BasicGallery from '~/components/pro/gallery/BasicGallery.vue';
 import HeadingDesigns1 from '~/components/pro/HeadingDesigns/HeadingDesigns1.vue';
 import HeadingDesigns2 from '~/components/pro/HeadingDesigns/HeadingDesigns2.vue';
+import DividerComponent from '~/components/pro/DividerComponent.vue';
+import FormComponent from '~/components/pro/forms/FormComponent.vue';
 </script>
 <template>
   <div class="mx-auto h-auto w-full max-w-4xl">
@@ -23,8 +25,10 @@ import HeadingDesigns2 from '~/components/pro/HeadingDesigns/HeadingDesigns2.vue
     <!-- <SectionsComponent /> -->
     <!-- <BasicGallery /> -->
     <!-- <LightBoxGallery /> -->
-    <HeadingDesigns1 />
+    <!-- <HeadingDesigns1 /> -->
     <!-- <HeadingDesigns2 /> -->
+    <DividerComponent />
+    <FormComponent />
 
     <!-- </div> -->
   </div>
