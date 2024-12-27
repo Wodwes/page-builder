0c25b0a4f8d33b90ef475898e441c63bdf15735d
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-27 11:19:36 +0500

    add icon component

---
+++ components/pro/IconComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+import { Icon } from '@iconify/vue';
+
+const props = defineProps({
+  icon: { default: 'material-symbols-light:star' },
+  styleClasses: { default: 'bg-gary-500' },
+  height: { default: '30' },
+  width: { default: '30' },
+});
+</script>
+
+<template>
+  <div :class="props.styleClasses">
+    <!-- use iconify icon   -->
+    <Icon :icon="props.icon" :width="props.width" :height="props.height" :ssr="true" />
+  </div>
+</template>
