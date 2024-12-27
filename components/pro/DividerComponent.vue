dbcd4e1842b23e7721a0ece77a00113b099ef95a
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-27 11:18:48 +0500

    add divider component

---
+++ components/pro/DividerComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  height: { default: 'h-px' },
+  width: { default: 'w-full' },
+  color: { default: 'bg-gray-300' },
+  margin: { default: 'my-4' },
+});
+</script>
+
+<template>
+  <div :class="[height, width, color, margin]"></div>
+</template>
