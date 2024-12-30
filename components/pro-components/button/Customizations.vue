af35708b359b34bdc91b77e84b0ffe37cd7cf449
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-30 14:52:54 +0500

    update

--- components/pro-components/button/Customizations.vue
+++ components/pro-components/button/Customizations.vue
@@ -4,24 +4,25 @@
       <h3 class="mb-1 text-sm">Size</h3>
       <USelect v-model="data.size" :items="textSizes" placeholder="Size" class="w-full" />
     </div>
-    <div class="">
+    <div>
       <h3 class="mb-1 text-sm">Style</h3>
       <USelect v-model="data.style" :items="textStyles" placeholder="Style" class="w-full" />
     </div>
-    <div class="">
+    <div>
       <h3 class="mb-1 text-sm">Weight</h3>
       <USelect v-model="data.weight" :items="textWeights" placeholder="Weight" class="w-full" />
     </div>
-    <div class="">
+    <div>
       <h3 class="mb-1 text-sm">display</h3>
       <USelect v-model="data.display" :items="buttonDispaly" placeholder="display" class="w-full" />
     </div>
-    <div class="">
+    <div>
       <h3 class="mb-1 text-sm">Alignment</h3>
       <USelect v-model="data.alignment" :items="textAlignment" placeholder="Alignment" class="w-full" />
     </div>
   </div>
-  <div class="">
+
+  <div>
     <h3 class="mb-1 text-sm">Title</h3>
     <UInput v-model="data.text" class="w-full" placeholder="Text" />
   </div>
